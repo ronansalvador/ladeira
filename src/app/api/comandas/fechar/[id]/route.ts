79bb8217d3c32b1b0e7da481c64d4489809bf059
Ronan Salvador <ronansalvador@yahoo.com.br>
2025-09-22 17:54:33 -0300

    fix: comandas/fechar/[id]

--- src/app/api/comandas/fechar/[id]/route.ts
+++ src/app/api/comandas/fechar/[id]/route.ts
@@ -1,44 +1,52 @@
 import { prisma } from '@/lib/prisma'
-import { NextResponse, NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(
   req: NextRequest,
-  { params }: { params: { id: string } },
+  context: { params: Promise<{ id: string }> },
 ) {
-  const comandaId = parseInt(params.id)
+  try {
+    const { id } = await context.params
+    const comandaId = parseInt(id)
 
-  const comanda = await prisma.comanda.findUnique({
-    where: { id: comandaId },
-    include: {
-      consumos: { include: { produto: true } },
-    },
-  })
+    const comanda = await prisma.comanda.findUnique({
+      where: { id: comandaId },
+      include: {
+        consumos: { include: { produto: true } },
+      },
+    })
 
-  if (!comanda) {
+    if (!comanda) {
+      return NextResponse.json(
+        { error: 'Comanda não encontrada' },
+        { status: 404 },
+      )
+    }
+
+    const totalConsumo = comanda.consumos.reduce(
+      (acc, c) => acc + c.quantidade * c.produto.preco,
+      0,
+    )
+
+    const valorEntrada =
+      comanda.tipoEntrada === 'vip'
+        ? 0
+        : comanda.tipoEntrada === 'antecipado'
+        ? 25
+        : 35
+
+    const total = valorEntrada + totalConsumo
+
+    const fechada = await prisma.comanda.update({
+      where: { id: comandaId },
+      data: { status: 'fechada', closedAt: new Date(), valorTotal: total },
+    })
+
+    return NextResponse.json(fechada)
+  } catch (error) {
     return NextResponse.json(
-      { error: 'Comanda não encontrada' },
-      { status: 404 },
+      { error: 'Erro ao fechar comanda', details: error },
+      { status: 500 },
     )
   }
-
-  const totalConsumo = comanda.consumos.reduce(
-    (acc, c) => acc + c.quantidade * c.produto.preco,
-    0,
-  )
-
-  const valorEntrada =
-    comanda.tipoEntrada === 'vip'
-      ? 0
-      : comanda.tipoEntrada === 'antecipado'
-      ? 25
-      : 35
-
-  const total = valorEntrada + totalConsumo
-
-  const fechada = await prisma.comanda.update({
-    where: { id: comandaId },
-    data: { status: 'fechada', closedAt: new Date(), valorTotal: total },
-  })
-
-  return NextResponse.json(fechada)
 }
